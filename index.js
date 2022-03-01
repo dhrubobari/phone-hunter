32e8e257fb223f5ce6e61b71f5c246938a5e4e6c
dhrubobari <bari.dhrubo@gmail.com>
2022-03-02 05:07:02 +0600

    card image added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const searchPhone = () => {
     const searchText = searchField.value;
     searchText.value = '';
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.data))
@@ -11,20 +11,20 @@ const searchPhone = () => {
 
 const displaySearchResult = phones => {
     // console.log(phones)
-const searchResult = document.getElementById('search-result')
+const searchResults = document.getElementById('search-result')
 phones.forEach(phone => {
     console.log(phone);
     const div = document.createElement('div')
     div.classList.add('col')
     div.innerHTML = `
     <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
           </div>
     `;
-    searchResult.appendChild(div);
+    searchResults.appendChild(div);
 })
 }
