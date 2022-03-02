ef57c778795d0ace0be132126595f0d98fcfa274
dhrubobari <bari.dhrubo@gmail.com>
2022-03-02 22:30:19 +0600

    last commit

--- index.js
+++ index.js
@@ -12,21 +12,43 @@ const searchPhone = () => {
 const displaySearchResult = phones => {
     // console.log(phones)
 const searchResults = document.getElementById('search-result')
+searchResults.innerHTML = '';
 phones.forEach(phone => {
-    console.log(phone);
+    // console.log(phone);
     const div = document.createElement('div')
     div.classList.add('col')
     div.innerHTML = `
-    <div class="card h-100">
+    <div onclick="showPhoneDetail(${phone.mainFeatures})" class="card h-100">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${phone.brand}</h5>
               <p class="card-text">${phone.phone_name}</p>
-              <p class="card-text">${phone.slug}</p>
-              <a href="#" class="btn btn-primary">Details</a>
+              <a href="" class="btn btn-primary">Details</a>
             </div>
           </div>
     `;
     searchResults.appendChild(div);
 })
+}
+
+const showPhoneDetail = id => {
+    // console.log(id)
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+}
+
+const displayDetails = phone => {
+    // console.log(phone)
+    const phoneDetails = document.getElementById('phone-details')
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <div class="card-body">
+        <h5 class="card-title">${phone.releaseDate}</h5>
+        <p class="card-text">${phone.sensors}</p>
+    </div>     
+    `;
+    phoneDetails.appendChild(div);
 }
